dd5c01e90734624bd52e38285fc6b16dce464768
Michael McIntire <mmcintire96>
2019-01-08 20:14:46 -0700

    more updates

--- classifier.py
+++ classifier.py
@@ -1,13 +1,35 @@
 import numpy as np
 import tensorflow as tf
 import paho.mqtt.client as mqtt
+import sqlite3
+
 
 MQTT_SERVER = 'localhost'
 MQTT_PATH = 'test_img'
 
 
-#helper function for running with an imgfile/off RPI
-def img_to_tensor():
+def add_to_db(output_obj):
+    conn = sqlite3.connect('cardata.db')
+    c = conn.cursor()
+    car_left = output_obj['car_left']
+    car_right = output_obj['car_right']
+    no_car = output_obj['no_car']
+    lot_id = output_obj['lot_id']
+    #this should be modified to insert car + or car - values to the lot_id
+    if car_left > .5:
+        print('car has entered the lot')
+    elif car_right > .5:
+        print('car has left the lot')
+    elif no_car > .5:
+        print('That was not a car')
+    c.execute("INSERT INTO location (car_left, car_right, no_car, lot_id,) values (?,?,?,?)", (str(car_left), str(car_right), str(no_car), str(lot_id)))
+    conn.commit()
+    conn.close()
+
+
+
+
+def img_to_tensor(lotid):
 
     # tf read file
     img_path = 'mqtt_img/output.jpg'
@@ -17,26 +39,29 @@ def img_to_tensor():
     image_reader = tf.image.decode_jpeg(file_reader, channels=3, name='jpeg_reader')
 
     #convert to tf.float, dim, resize [299,299] and normalize
+    # maybe make into b/w to match trained data
     float_caster = tf.cast(image_reader, tf.float32)
     dims_expander = tf.expand_dims(float_caster, 0)
     resized = tf.image.resize_bilinear(dims_expander, [299, 299])
     normalized = tf.divide(tf.subtract(resized, [0]), [255])
 
-    make_prediction(normalized)
+    make_prediction(normalized, lotid)
 
 
-def make_prediction(normalized):
+def make_prediction(normalized, lotid):
+
     with open('model_outputs/output_labels.txt', 'r') as label_file:
         labels = [line.strip('\n') for line in label_file]
     labels = labels
-    # load model
+
     with tf.gfile.FastGFile('model_outputs/output_graph.pb', 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
-    # get classification
+
     with tf.Session() as sess:
-        #convert to 4-d np.array[batch_size, height, width, channel] 
+        #convert the normalized arr to 4-d np.array[batch_size, height, width, channel] 
+        # maybe do this in img_to_tensor()?
         img = sess.run(normalized)
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         predictions = sess.run(softmax_tensor, {'Placeholder:0': img})
@@ -49,8 +74,13 @@ def make_prediction(normalized):
 
         print(predictions, labels)
         print(("%s (%.2f%%)" % (predicted_label, max_value)))
-
-
+        output_obj = {
+            'car_left': predictions[0][0],
+            'car_right': predictions[0][1],
+            'no_car': predictions[0][2],
+            'lot_id': lotid
+        }
+        add_to_db(output_obj)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected : " +str(rc))
@@ -63,7 +93,7 @@ def on_message(client, userdata, msg):
     f = open("mqtt_img/output.jpg", "wb")
     f.write(msg.payload)
     f.close()
-    img_to_tensor()
+    img_to_tensor(msg.topic)
 
 
 client = mqtt.Client()
